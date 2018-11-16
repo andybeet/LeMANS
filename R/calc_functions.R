c7407569656c91f58f4429ac00c072f496edb331
Andrew Beet <Andrew.Beet@nmfs.local>
2018-11-16 11:14:43 -0500

    track calc_functions file

---
+++ R/calc_functions.R
@@ -0,0 +1,18 @@
+
+# utility functions unlikely to change
+
+
+# Calculates the proportion of individuals that leave
+# each size class. Output is scaled relative to the
+# species size class combinations which is emptied fastest
+# so that this combination empties completely in one timestep.
+#
+# ie scaled to fasted growing individual
+calc_phi <- function(nSize,nSpecies){
+
+  parameterValues$k
+  parameterValues$Linf
+
+  return(10)
+}
+
