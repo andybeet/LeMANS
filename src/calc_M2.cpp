fdc84c08da4e6a55f9f7f53c9232b54357c2d2f2
Andrew Beet <andrew.beet>
2018-11-26 17:46:28 -0500

    define output type rather than SEXP. Degugging code and fstream to examine output

--- src/calc_M2.cpp
+++ src/calc_M2.cpp
@@ -1,13 +1,22 @@
+// calculates M2 using c++
+//
+// @param loads of parameters. document later
+//
 
 #include <RcppArmadillo.h>
+#include <fstream>
+
 // [[Rcpp::depends(RcppArmadillo)]]
 
 using namespace std;
 
 // [[Rcpp::export]]
-SEXP calc_M2(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma::mat ration, arma::mat wgt, arma::cube suitability ,double phiMin, double otherFood ) {
+arma::mat calc_M2(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma::mat ration, arma::mat wgt, arma::cube suitability ,double phiMin, double otherFood ) {
 
+  std::ofstream ofs;
+  ofs.open ("test.txt", std::ofstream::out | std::ofstream::app);
   arma::mat M2 = arma::zeros(nSize,nSpecies);
+
 
   for (int msp = 0; msp<nSpecies; msp++) { //prey species
     for (int nsz = 0; nsz < scLinf(msp); nsz++){ // prey size
@@ -15,6 +24,8 @@ SEXP calc_M2(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma::mat r
         for (int jsz = 0; jsz < scLinf(isp); jsz++) { //predator size
           int index = (isp * nSize) + jsz;
           double numerator = ration(jsz,isp)*N(jsz,isp)*suitability(index,nsz,msp);
+
+          Rcpp::Rcout << "numerator = " << numerator << std::endl;
           // additional loops to sum over suitabilities
           arma::mat tempSuit = arma::zeros(nSize,nSpecies);
           for (int lsp=0 ; lsp < nSpecies ; lsp++) {
@@ -23,8 +34,12 @@ SEXP calc_M2(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma::mat r
             }
           }
           double denominator = accu(tempSuit % wgt % N) + otherFood;
+          Rcpp::Rcout << "denominator = " << denominator << std::endl;
+          ofs << msp<< nsz<< isp<< jsz <<" "<<numerator << "\n";
+          ofs << msp<< nsz<< isp<< jsz <<" "<<denominator << "\n";
 
-          M2(nsz,msp) += numerator/denominator;
+
+          M2(nsz,msp) += (numerator/denominator);
 
         }
 
@@ -32,32 +47,18 @@ SEXP calc_M2(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma::mat r
     }
 
   }
+  ofs.close();
+  arma::mat test = arma::zeros(4,4);
+  test = (test + 3)/2;
+  Rcpp::Rcout << test <<std::endl;
+  M2 = M2/phiMin;
+//M2 = 10 * nSize;
+  Rcpp::Rcout << "This is dim of M2 = " << M2.size() << std::endl;
 
-  M2 = 10 * nSize;
-  Rcpp::Rcout << "This is nSize = " << nSize << std::endl;
-
-  return Rcpp::wrap(M2);
+  return M2;
 }
 
 
 // this checks to see if user has hit ctrl + c to abort script
 // Rcpp::checkUserInterrupt()
 
-// calc_M2 <- function(nSize,nSpecies,N,ration,suitability,phiMin,otherFood){
-//
-//   M2 <- matrix(data=0,nrow = nSize,ncol = nSpecies)
-//   for (msp in 1:nSpecies) {  # prey species
-//     for (nsz in 1:ration$scLinf[msp]) { # prey size class
-//       for (isp in 1:nSpecies) { # predator
-//         for (jsz in 1:ration$scLinf[isp]) { # predator size
-//           numerator <- ration$ration[jsz,isp]*N[jsz,isp]*suitability[jsz,isp,nsz,msp]
-//           denominator <- sum(suitability[jsz,isp,,] * ration$wgt * N) + otherFood
-//           M2[nsz,msp] <- M2[nsz,msp] + numerator/denominator
-//         }
-//       }
-//     }
-//   }
-//   M2 <- M2*phiMin
-//
-//     return(M2)
-// }
