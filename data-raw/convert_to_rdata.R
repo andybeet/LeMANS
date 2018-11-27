774986eab5681dd17e475457b0a62cc6b31e444e
Andrew Beet <Andrew.Beet@nmfs.local>
2018-11-27 18:19:52 -0500

    data raw export code data prefix

--- data-raw/convert_to_rdata.R
+++ data-raw/convert_to_rdata.R
@@ -4,26 +4,26 @@ convert_to_rdata <- function() {
   path <- paste0("data-raw/")
 
   #reads in species list
-  species <- read.csv(paste0(path,"speciesID.csv"),header=TRUE)
+  data_species <- read.csv(paste0(path,"speciesID.csv"),header=TRUE)
   # reads in Foodweb structure
-  foodweb <- read.table(paste0(path,"GB_KeyRun_FW.dat"))
-  names(foodweb) <- species$commonName
-  row.names(foodweb) <- species$commonName
+  data_foodweb <- read.table(paste0(path,"GB_KeyRun_FW.dat"))
+  names(data_foodweb) <- data_species$commonName
+  row.names(data_foodweb) <- data_species$commonName
 
   # read in initial values
-  initialValues <- read.table(paste0(path,"GB_KeyRun_InitVal.dat"))
-  row.names(initialValues) <- species$commonName
+  data_initialValues <- read.table(paste0(path,"GB_KeyRun_InitVal.dat"))
+  row.names(data_initialValues) <- data_species$commonName
 
   # read in parameter values
-  parameterValues <- read.table(paste0(path,"GB_KeyRun_Params_SMAX2011_Linf.dat"))
-  row.names(parameterValues) <- species$commonName
-  names(parameterValues) <- c("k","Linf","Lmat","kappa","wa","wb","Smax","IsFished")
+  data_parameterValues <- read.table(paste0(path,"GB_KeyRun_Params_SMAX2011_Linf.dat"))
+  row.names(data_parameterValues) <- data_species$commonName
+  names(data_parameterValues) <- c("k","Linf","Lmat","kappa","wa","wb","Smax","IsFished")
 
 
-  save(species,file="data/species.RData")
-  save(foodweb,file="data/foodweb.RData")
-  save(initialValues,file="data/initialValues.RData")
-  save(parameterValues,file="data/parameterValues.RData")
+  save(data_species,file="data/data_species.RData")
+  save(data_foodweb,file="data/data_foodweb.RData")
+  save(data_initialValues,file="data/data_initialValues.RData")
+  save(data_parameterValues,file="data/data_parameterValues.RData")
 
 
 
