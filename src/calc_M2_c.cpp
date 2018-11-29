7e2f286a8f7a4a28d0d3977bdda7feeac779dda3
Andrew Beet <Andrew.Beet@nmfs.local>
2018-11-29 10:24:44 -0500

    fixed issue with calc_M2_c roxygen comments and export issues

--- src/calc_M2_c.cpp
+++ src/calc_M2_c.cpp
@@ -1,18 +1,31 @@
-// calculates M2 using c++
-//
-// @param loads of parameters. document later
-//@export
-
-// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rccp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
-//#include <fstream>
-
-
-// this checks to see if user has hit ctrl + c to abort script. Add it to the body of code
-// Rcpp::checkUserInterrupt()
-
-using namespace std;
-
+//' calculates M2 using c++
+//'
+//' Calculates the predation mortality for each species in each size class
+//'
+//'@param nSize Number of size class intervals species can grow through
+//'@param nSpecies Number of species in the model
+//'@param N nSize x nSpecies matrix of abundance (number of individuals)
+//'@param scLinf The size class at which each species reaches L_inf (maximum length)
+//'@param ration nSize x nSpecies matrix. (growth in time interval)/growth efficiency. See \code{\link{calc_ration}}
+//'@param wgt Weight of species at the mid point of each size class (Units: grams). See \code{\link{calc_ration}}
+//'@param suitability 3D array of predator size preference of prey size. See \code{\link{calc_sizepref_suitability}}
+//'@param phiMin Model timestep (years). See \code{\link{calc_phi}}
+//'@param otherFood Amount of other food available not accounted for in the model (g)
+//'
+//'@return A matrix is returned
+//'
+//'   \code{M2}    - nSize x nSpecies matrix of M2 (predation mortality) values.
+//'    Note that M2_i,j = 0 for size classes i in which species j is not preyed upon.
+//'
+//'@seealso \code{\link{calc_sizepref_suitability}}, \code{\link{calc_ration}} \code{\link{calc_phi}}
+//'@section References:
+//'Hall et al. (2006). A length-based multispecies model for evaluating community responses to fishing. Can. J. Fish. Aquat. Sci. 63:1344-1359.
+//'
+//'Rochet et al. (2011). Does selective fishing conserve community biodiversity? Prediction from a length-based multispecies model. Can. J. Fish. Aquat. Sci. 68:469-486
+//'alc_
+//' @export
 // [[Rcpp::export]]
 arma::mat calc_M2_c(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma::mat ration, arma::mat wgt,
                   arma::cube suitability, double phiMin, double otherFood) {
@@ -21,13 +34,14 @@ arma::mat calc_M2_c(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma
 
 
   for (int msp = 0; msp<nSpecies; msp++) { //prey species
+    // this checks to see if user has hit ctrl + c to abort script. Add it to the body of code
+    Rcpp::checkUserInterrupt();
     for (int nsz = 0; nsz < scLinf(msp); nsz++){ // prey size
       for (int isp=0; isp<nSpecies; isp++) { //predator species
         for (int jsz = 0; jsz < scLinf(isp); jsz++) { //predator size
           int index = (isp * nSize) + jsz;
           double numerator = ration(jsz,isp)*N(jsz,isp)*suitability(index,nsz,msp);
 
-          //Rcpp::Rcout << "numerator = " << numerator << std::endl;
           // additional loops to sum over suitabilities
           arma::mat tempSuit = arma::zeros(nSize,nSpecies);
           for (int lsp=0 ; lsp < nSpecies ; lsp++) {
@@ -36,7 +50,6 @@ arma::mat calc_M2_c(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma
             }
           }
           double denominator = accu(tempSuit % wgt % N) + otherFood;
-          //Rcpp::Rcout << "denominator = " << denominator << std::endl;
 
           M2(nsz,msp) += (numerator/denominator);
 
@@ -45,12 +58,7 @@ arma::mat calc_M2_c(int nSize, int nSpecies, arma::mat N, arma::vec scLinf, arma
     }
   }
 
-
   M2 = M2*phiMin;
-  //M2 = M2/4.58;
-
-  //Rcpp::Rcout << "This is dim of M2 = " << M2.size() << std::endl;
-
   return M2;
 }
 
